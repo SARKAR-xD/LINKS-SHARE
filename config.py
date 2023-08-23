931c8a1b543c24c5c42d21cf1c7404ebd9b251aa
AADI-001 <131204586+AADI-001@users.noreply.github.com>
2023-08-23 22:22:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,60 +13,40 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "5402921"))
+APP_ID = int(os.environ.get("APP_ID", ""))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "6ba38086ea0265fcf11b2d951bc47fda")
+API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "1407122915"))
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Tosumemon:Tosumemon@cluster0.wnpcxp8.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001407122915"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first} & Welcome To [TIF] Links Bot
-
-📌Join our other Channels
-@TIF_TvSeries1 🌹 @TIF_WebSeries 
-@TIF_Moviez 🌹 @TIF_Anime
-
-📌Our Discussion Group
-@TIFDiscuss 🌹 @TIF_OTs 🌹 @TIFRequests
-
-📌Our Store
-@TIF_Shoppie 🌹 @TIF_Vouches
-
-📌 Follow us on Instagram 
-https://instagram.com/tif_network?igshid=YmMyMTA2M2Y=")
+START_MSG = os.environ.get("START_MESSAGE", "")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1014734769 692926828 878419316 992923461 1232710616 989451380
-994884531 1548692630 1372828370 1428191214 1399513864 1059440698
-1141611406 846915783 1365187878  772094614 1842073219 724516516 1988210787 1626129666 1545262687 838912777  1250858488 1106954355 1170258624 1066681358 1626419978  831587397 1345552722 2129023041 2001837805").split()):
+    for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}
-
-<b>To use me, You need to join 
-"[TIF] Network @TIF_Network"
-
-Join the Channel and press 'start' again</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
